d3814c95c42870b491bfe1eb0e8ffa08e855072c
zawiszaty <ZawiszaCzarny52@gmail.com>
2019-08-28 10:06:13 +0200

    change .env path

--- src/Config/EnvLoader.go
+++ src/Config/EnvLoader.go
@@ -7,7 +7,7 @@ import (
 )
 
 func GetEnv(env string) string {
-	err := godotenv.Load()
+	err := godotenv.Load("/go/src/go_books/src/.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
